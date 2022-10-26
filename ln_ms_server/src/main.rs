99263dd10dbedfa6bc4cb4f8e5197ab70888dd5b
Blake Johnson <brj1993@icloud.com>
2022-10-26 17:22:53 -0500

    Setting up an initial web server

--- ln_ms_server/src/main.rs
+++ ln_ms_server/src/main.rs
@@ -1,3 +1,37 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+use ln_ms_lib;
+
+const HTML_STRING: &str = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\"><title>Hello!</title></head><body><h1>Hello!</h1><p>Hi from Rust</p></body></html>";
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection established!");
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let _http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let num = ln_ms_lib::add(2, 3);
+    println!("ln_ms_lib used: {num}");
+
+    let status_line = "HTTP/1.1 200 OK";
+    let length = HTML_STRING.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{HTML_STRING}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
